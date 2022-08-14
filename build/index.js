a81cc58ecd68bc6da0b5af229eba76bba4e7b413
Eva Vetrova <yevavetrova@gmail.com>
2022-08-15 00:23:53 +0300

    WIP

--- build/index.js
+++ build/index.js
@@ -28,6 +28,26 @@ const React = __importStar(require("react"));
 const si = __importStar(require("systeminformation"));
 const logos_1 = require("./logos");
 const App = () => {
+    const [OS, setOS] = React.useState('N/A');
+    const [kernel, setKernel] = React.useState('N/A');
+    const [host, setHost] = React.useState('N/A');
+    const [shell, setShell] = React.useState('N/A');
+    const [resolution, setResolution] = React.useState(['N/A']);
+    const [CPU, setCPU] = React.useState('N/A');
+    const [GPU, setGPU] = React.useState(['N/A']);
+    const [RAM, setRAM] = React.useState('N/A');
+    const [logo, setLogo] = React.useState('N/A');
+    const sysinfo = {
+        OS,
+        kernel,
+        host,
+        shell,
+        resolution,
+        CPU,
+        GPU,
+        RAM,
+        logo,
+    };
     // Source https://stackoverflow.com/a/18650828
     const formatBytes = (bytes, decimals = 2) => {
         if (bytes === 0)
@@ -38,79 +58,77 @@ const App = () => {
         const i = Math.floor(Math.log(bytes) / Math.log(k));
         return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     };
-    const [OS, setOS] = React.useState('N/A');
-    const [kernel, setKernel] = React.useState('N/A');
-    const [host, setHost] = React.useState('N/A');
-    const [shell, setShell] = React.useState('N/A');
-    const [resolution, setResolution] = React.useState('N/A');
-    const [CPU, setCPU] = React.useState('N/A');
-    const [GPU, setGPU] = React.useState('N/A');
-    const [RAM, setRAM] = React.useState('N/A');
-    const [logo, setLogo] = React.useState('');
-    const sysinfo = {
-        OS,
-        kernel,
-        host,
-        shell,
-        resolution,
-        CPU,
-        GPU,
-        RAM,
-        logo
-    };
     const getLogo = () => {
         switch (logo) {
             case 'debian':
             case 'gentoo':
             case 'mint':
+            case 'fedora':
+            case 'mac os':
                 return logos_1.logos[logo];
         }
+    };
+    // Source: https://stackoverflow.com/a/52387803
+    const secondsToDhms = (seconds) => {
+        const d = Math.floor(seconds / (3600 * 24));
+        const h = Math.floor(seconds % (3600 * 24) / 3600);
+        const m = Math.floor(seconds % 3600 / 60);
+        const dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : "";
+        const hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
+        const mDisplay = m > 0 ? m + (m === 1 ? " minute " : " minutes ") : "";
+        return dDisplay + hDisplay + mDisplay;
     };
     si.osInfo().then(data => {
         setOS(data.distro);
         setKernel(data.kernel);
         setHost(data.hostname);
         setLogo(data.logofile);
-    })
-        .catch(e => console.log(e));
-    si.shell().then(data => setShell(data))
-        .catch(e => console.log(e));
-    si.graphics().then(data => setResolution(`${data.displays[0].resolutionX}x${data.displays[0].resolutionY}`))
-        .catch(e => console.log(e));
-    si.cpu().then(data => setCPU(`${data.manufacturer} ${data.brand} ${data.speed}`))
-        .catch(e => console.log(e));
-    si.graphics().then(data => setGPU(`${data.controllers[0].vendor} ${data.controllers[0].model}`))
-        .catch(e => console.log(e));
-    si.mem().then(data => setRAM(`${formatBytes(data.used)}/${formatBytes(data.total)}`))
-        .catch(e => console.log(e));
-    return (React.createElement(React.Fragment, null,
-        React.createElement(Ink.Box, { flexDirection: 'row', paddingLeft: 1, paddingTop: 1, paddingBottom: 1 },
-            React.createElement(Ink.Box, { paddingRight: 2 },
-                React.createElement(Ink.Text, null, getLogo())),
-            React.createElement(Ink.Box, { flexDirection: 'column' },
-                React.createElement(Ink.Box, { flexDirection: 'row' },
-                    React.createElement(Ink.Text, { color: 'blueBright' }, "OS: "),
-                    React.createElement(Ink.Text, null, sysinfo.OS)),
-                React.createElement(Ink.Box, { flexDirection: 'row' },
-                    React.createElement(Ink.Text, { color: 'blueBright' }, "Host: "),
-                    React.createElement(Ink.Text, null, sysinfo.host)),
-                React.createElement(Ink.Box, { flexDirection: 'row' },
-                    React.createElement(Ink.Text, { color: 'blueBright' }, "Kernel: "),
-                    React.createElement(Ink.Text, null, sysinfo.kernel)),
-                React.createElement(Ink.Box, { flexDirection: 'row' },
-                    React.createElement(Ink.Text, { color: 'blueBright' }, "Shell: "),
-                    React.createElement(Ink.Text, null, sysinfo.shell)),
-                React.createElement(Ink.Box, { flexDirection: 'row' },
-                    React.createElement(Ink.Text, { color: 'blueBright' }, "Resolution: "),
-                    React.createElement(Ink.Text, null, sysinfo.resolution)),
-                React.createElement(Ink.Box, { flexDirection: 'row' },
-                    React.createElement(Ink.Text, { color: 'blueBright' }, "CPU: "),
-                    React.createElement(Ink.Text, null, sysinfo.CPU)),
-                React.createElement(Ink.Box, { flexDirection: 'row' },
-                    React.createElement(Ink.Text, { color: 'blueBright' }, "GPU: "),
-                    React.createElement(Ink.Text, null, sysinfo.GPU)),
-                React.createElement(Ink.Box, { flexDirection: 'row' },
-                    React.createElement(Ink.Text, { color: 'blueBright' }, "RAM: "),
-                    React.createElement(Ink.Text, null, sysinfo.RAM))))));
+    });
+    // .catch(e => console.log(e));
+    si.shell().then(data => setShell(data));
+    // .catch(e => console.log(e));
+    si.graphics().then(data => setResolution(data.displays.map(item => `${item.resolutionX}x${item.resolutionY}`)))
+        .catch(e => setResolution([e]));
+    si.cpu().then(data => setCPU(`${data.manufacturer} ${data.brand} ${data.speed}`));
+    // .catch(e => console.log(e));
+    si.graphics().then(data => setGPU(data.controllers.map(item => `${item.vendor} ${item.model}`)));
+    // .catch(e => console.log(e));
+    si.mem().then(data => setRAM(`${formatBytes(data.used)} / ${formatBytes(data.total)}`));
+    // .catch(e => console.log(e));
+    return (React.createElement(Ink.Box, { flexDirection: 'row', paddingLeft: 1, paddingTop: 1, paddingBottom: 1 },
+        React.createElement(Ink.Box, { paddingRight: 2 },
+            React.createElement(Ink.Text, null, getLogo())),
+        React.createElement(Ink.Box, { flexDirection: 'column', paddingTop: 5 },
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "OS: "),
+                React.createElement(Ink.Text, null, sysinfo.OS)),
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "Host: "),
+                React.createElement(Ink.Text, null, sysinfo.host)),
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "Kernel: "),
+                React.createElement(Ink.Text, null, sysinfo.kernel)),
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "Shell: "),
+                React.createElement(Ink.Text, null, sysinfo.shell)),
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "Resolution: "),
+                sysinfo.resolution.length > 1
+                    ? React.createElement(Ink.Text, null, `${sysinfo.resolution.join('\n')}`)
+                    : React.createElement(Ink.Text, null, sysinfo.resolution)),
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "CPU: "),
+                React.createElement(Ink.Text, null, sysinfo.CPU)),
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "GPU: "),
+                sysinfo.GPU.length > 1
+                    ? React.createElement(Ink.Text, null, `${sysinfo.GPU.join('\n')}`)
+                    : React.createElement(Ink.Text, null, sysinfo.GPU)),
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "RAM: "),
+                React.createElement(Ink.Text, null, sysinfo.RAM)),
+            React.createElement(Ink.Box, { flexDirection: 'row' },
+                React.createElement(Ink.Text, { color: 'blueBright' }, "Uptime: "),
+                React.createElement(Ink.Text, null, typeof (si.time().uptime) ? secondsToDhms(si.time().uptime) : 'N/A')))));
 };
 Ink.render(React.createElement(App, null));
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,27 @@ import * as si from 'systeminformation';
 import {logos as logoList} from './logos';
 
 const App = (): JSX.Element => {
+    const [OS, setOS] = React.useState<string>('N/A');
+    const [kernel, setKernel] = React.useState<string>('N/A');
+    const [host, setHost] = React.useState<string>('N/A');
+    const [shell, setShell] = React.useState<string>('N/A');
+    const [resolution, setResolution] = React.useState<string[]>(['N/A']);
+    const [CPU, setCPU] = React.useState<string>('N/A');
+    const [GPU, setGPU] = React.useState<string[]>(['N/A']);
+    const [RAM, setRAM] = React.useState<string>('N/A');
+    const [logo, setLogo] = React.useState<string>('N/A');
 
+    const sysinfo: ISysInfo = {
+        OS,
+        kernel,
+        host,
+        shell,
+        resolution,
+        CPU,
+        GPU,
+        RAM,
+        logo,
+    };
 
     // Source https://stackoverflow.com/a/18650828
 
@@ -20,36 +40,28 @@ const App = (): JSX.Element => {
         return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     }
 
-
-    const [OS, setOS] = React.useState<string>('N/A');
-    const [kernel, setKernel] = React.useState<string>('N/A');
-    const [host, setHost] = React.useState<string>('N/A');
-    const [shell, setShell] = React.useState<string>('N/A');
-    const [resolution, setResolution] = React.useState<string>('N/A');
-    const [CPU, setCPU] = React.useState<string>('N/A');
-    const [GPU, setGPU] = React.useState<string>('N/A');
-    const [RAM, setRAM] = React.useState<string>('N/A');
-    const [logo, setLogo] = React.useState<string>('')
-
-    const sysinfo: ISysInfo = {
-        OS,
-        kernel,
-        host,
-        shell,
-        resolution,
-        CPU,
-        GPU,
-        RAM,
-        logo
-    };
-
     const getLogo = () => {
         switch (logo) {
             case 'debian':
             case 'gentoo':
             case 'mint':
+            case 'fedora':
+            case 'mac os':
                 return logoList[logo];
         }
+    }
+
+    // Source: https://stackoverflow.com/a/52387803
+
+    const secondsToDhms = (seconds: number) => {
+        const d = Math.floor(seconds / (3600*24));
+        const h = Math.floor(seconds % (3600*24) / 3600);
+        const m = Math.floor(seconds % 3600 / 60);
+
+        const dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : "";
+        const hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
+        const mDisplay = m > 0 ? m + (m === 1 ? " minute " : " minutes ") : "";
+        return dDisplay + hDisplay + mDisplay;
     }
 
     si.osInfo().then(data => {
@@ -58,78 +70,87 @@ const App = (): JSX.Element => {
         setHost(data.hostname);
         setLogo(data.logofile)
     })
-        .catch(e => console.log(e))
+        // .catch(e => console.log(e));
 
     si.shell().then(data => setShell(data))
-        .catch(e => console.log(e));
+        // .catch(e => console.log(e));
 
-    si.graphics().then(data => setResolution(`${data.displays[0].resolutionX}x${data.displays[0].resolutionY}`))
-        .catch(e => console.log(e));
+    si.graphics().then(data => setResolution(data.displays.map(item => `${item.resolutionX}x${item.resolutionY}`)))
+        .catch(e => setResolution([e]));
 
     si.cpu().then(data => setCPU(`${data.manufacturer} ${data.brand} ${data.speed}`))
-        .catch(e => console.log(e));
+        // .catch(e => console.log(e));
 
-    si.graphics().then(data => setGPU(`${data.controllers[0].vendor} ${data.controllers[0].model}`))
-        .catch(e => console.log(e));
+    si.graphics().then(data => setGPU(data.controllers.map(item => `${item.vendor} ${item.model}`)))
+        // .catch(e => console.log(e));
 
-    si.mem().then(data => setRAM(`${formatBytes(data.used)}/${formatBytes(data.total)}`))
-        .catch(e => console.log(e));
+    si.mem().then(data => setRAM(`${formatBytes(data.used)} / ${formatBytes(data.total)}`))
+        // .catch(e => console.log(e));
 
     return (
-        <>
-            <Ink.Box flexDirection='row' paddingLeft={1} paddingTop={1} paddingBottom={1}>
-                <Ink.Box paddingRight={2}>
-                    <Ink.Text>{getLogo()}</Ink.Text>
+        <Ink.Box flexDirection='row' paddingLeft={1} paddingTop={1} paddingBottom={1}>
+            <Ink.Box paddingRight={2}>
+                <Ink.Text>{getLogo()}</Ink.Text>
+            </Ink.Box>
+            <Ink.Box flexDirection='column' paddingTop={5}>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>OS: </Ink.Text>
+                    <Ink.Text>{sysinfo.OS}</Ink.Text>
                 </Ink.Box>
-                <Ink.Box flexDirection='column'>
-                    <Ink.Box flexDirection='row'>
-                        <Ink.Text color='blueBright'>OS: </Ink.Text>
-                        <Ink.Text>{sysinfo.OS}</Ink.Text>
-                    </Ink.Box>
-                    <Ink.Box flexDirection='row'>
-                        <Ink.Text color='blueBright'>Host: </Ink.Text>
-                        <Ink.Text>{sysinfo.host}</Ink.Text>
-                    </Ink.Box>
-                    <Ink.Box flexDirection='row'>
-                        <Ink.Text color='blueBright'>Kernel: </Ink.Text>
-                        <Ink.Text>{sysinfo.kernel}</Ink.Text>
-                    </Ink.Box>
-                    <Ink.Box flexDirection='row'>
-                        <Ink.Text color='blueBright'>Shell: </Ink.Text>
-                        <Ink.Text>{sysinfo.shell}</Ink.Text>
-                    </Ink.Box>
-                    <Ink.Box flexDirection='row'>
-                        <Ink.Text color='blueBright'>Resolution: </Ink.Text>
-                        <Ink.Text>{sysinfo.resolution}</Ink.Text>
-                    </Ink.Box>
-                    <Ink.Box flexDirection='row'>
-                        <Ink.Text color='blueBright'>CPU: </Ink.Text>
-                        <Ink.Text>{sysinfo.CPU}</Ink.Text>
-                    </Ink.Box>
-                    <Ink.Box flexDirection='row'>
-                        <Ink.Text color='blueBright'>GPU: </Ink.Text>
-                        <Ink.Text>{sysinfo.GPU}</Ink.Text>
-                    </Ink.Box>
-                    <Ink.Box flexDirection='row'>
-                        <Ink.Text color='blueBright'>RAM: </Ink.Text>
-                        <Ink.Text>{sysinfo.RAM}</Ink.Text>
-                    </Ink.Box>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>Host: </Ink.Text>
+                    <Ink.Text>{sysinfo.host}</Ink.Text>
+                </Ink.Box>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>Kernel: </Ink.Text>
+                    <Ink.Text>{sysinfo.kernel}</Ink.Text>
+                </Ink.Box>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>Shell: </Ink.Text>
+                    <Ink.Text>{sysinfo.shell}</Ink.Text>
+                </Ink.Box>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>Resolution: </Ink.Text>
+                    {sysinfo.resolution.length > 1
+                        ? <Ink.Text>{`${sysinfo.resolution.join('\n')}`}</Ink.Text>
+                        : <Ink.Text>{sysinfo.resolution}</Ink.Text>
+                    }
+                </Ink.Box>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>CPU: </Ink.Text>
+                    <Ink.Text>{sysinfo.CPU}</Ink.Text>
+                </Ink.Box>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>GPU: </Ink.Text>
+                    {sysinfo.GPU.length > 1
+                        ? <Ink.Text>{`${sysinfo.GPU.join('\n')}`}</Ink.Text>
+                        : <Ink.Text>{sysinfo.GPU}</Ink.Text>
+                    }
+                </Ink.Box>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>RAM: </Ink.Text>
+                    <Ink.Text>{sysinfo.RAM}</Ink.Text>
+                </Ink.Box>
+                <Ink.Box flexDirection='row'>
+                    <Ink.Text color='blueBright'>Uptime: </Ink.Text>
+                    <Ink.Text>{typeof(si.time().uptime) ? secondsToDhms(si.time().uptime) : 'N/A'}</Ink.Text>
                 </Ink.Box>
             </Ink.Box>
-        </>
+        </Ink.Box>
     );
 };
 
 Ink.render(<App />);
+
 
 interface ISysInfo {
     OS: string,
     host: string,
     kernel: string,
     shell: string,
-    resolution: string,
+    resolution: string[],
     CPU: string,
-    GPU: string,
+    GPU: string[],
     RAM: string,
     logo: string,
 }
